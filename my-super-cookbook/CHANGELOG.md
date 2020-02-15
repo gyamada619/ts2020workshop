0fd45b051df9dd15a14186e9a8d76fada9d73272
Garrett Yamada <gyamada619@gmail.com>
2020-02-15 12:20:22 -0600

    Adding cookbook, recipe, kitchen.yml

---
+++ my-super-cookbook/CHANGELOG.md
@@ -0,0 +1,11 @@
+# my-super-cookbook CHANGELOG
+
+This file is used to list changes made in each version of the my-super-cookbook cookbook.
+
+# 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
+
---
+++ my-super-cookbook/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: my-super-cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, Garrett Yamada, All Rights Reserved.
+
+# Let's create a file as a simple test of Chef.
+file 'C:\\example.txt' do
+  content 'Hello, Tech Summit!'
+  action :create
+end
+
+# Now we're going to copy our Chocolatey package to our new node.
+cookbook_file 'C:\\temp\\example-wireguard.0.0.38.nupkg' do
+  source 'example-wireguard.0.0.38.nupkg'
+  action :create
+end
+
+# Now that the package exists, let's install it.
+chocolatey_package 'example-wireguard' do
+  source 'C:\\temp\\'
+  action :install
+end
